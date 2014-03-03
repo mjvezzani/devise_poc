c9aae893466be8eefd8281f748bdd41610b1d16d
Michael Vezzani <mjvezzani@gmail.com>
2014-03-03 17:08:17 -0500

    Sign in/sign out/sign up all working as expected.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DevisePoc::Application.config.secret_key_base = 'ede5de77d88f778fb833fc4bf8e120a7ee5bf2e2627b9e4438dc666001f05b0af1fdc5b2df2efec1e52d8c669912895fb8e2dc406268918cc3cd5078b4472891'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DevisePoc::Application.config.session_store :cookie_store, key: '_devise_poc_session'
